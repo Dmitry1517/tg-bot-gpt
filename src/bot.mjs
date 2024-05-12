230af327a4de26248c05b7b86883329fe421d96b
Dmitry1517 <94603546+Dmitry1517@users.noreply.github.com>
2024-05-13 01:15:33 +1000

    Update bot.mjs

--- src/bot.mjs
+++ src/bot.mjs
@@ -3,6 +3,6 @@ import TeleBot from "telebot"
 
 const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
 
-bot.on("text", msg => msg.reply.text(msg.text))
+bot.on("text", msg => msg.reply.text("Скоро я научусть отвечать!!!"))
 
 export default bot
