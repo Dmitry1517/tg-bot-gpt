138d8c842c349e6d15b6304582cf6771b0660384
Dmitry1517 <postbox404@gmail.com>
2025-02-28 16:50:33 +1000

    bot-start

--- src/bot.mjs
+++ src/bot.mjs
@@ -3,7 +3,7 @@ import TeleBot from "telebot"
 
 const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
 
-bot.on(/\/start/, (msg) => {
+bot.on('/start', (msg) => {
   const chatId = msg.chat.id;
   bot.sendMessage(chatId, "Привет! Отправь мне ссылку на видео с YouTube, и я скачаю его для тебя.");
 });
